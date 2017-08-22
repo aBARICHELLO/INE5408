39246ff5c4ef26f652a9a181f74915483a6e7887
aBARICHELLO <arturbarichello@hotmail.com>
2017-08-22 09:39:56 -0300

    Incomplete array list

---
+++ array_list/array_list.h
@@ -0,0 +1,175 @@
+//  Copyright [2017] <Barichello>
+// 77.27/100.00
+
+#ifndef STRUCTURES_ARRAY_LIST_H
+#define STRUCTURES_ARRAY_LIST_H
+
+#include <cstdint>
+#include <stdexcept>
+
+namespace structures {
+
+template<typename T>
+/*!  ArrayList class */
+class ArrayList {
+ public:
+    //!  ArrayList constructor //
+    explicit ArrayList(std::size_t max_size_ = DEFAULT_MAX) :
+	   contents{new T[max_size_]}, max_size_{DEFAULT_MAX}, size_{0} {
+    }
+
+    //!  ArrayList destructor //
+    ~ArrayList() {
+        delete[] contents;
+    }
+
+    //!  Clears our array list //
+    void clear() {
+        size_ = 0;
+    }
+
+    //!  Inserts an element - back //
+    void push_back(const T& data) {
+        if (full())
+            throw std::out_of_range("Full!");
+        contents[size_++] = data;
+    }
+
+    //!  Inserts an element - front //
+    void push_front(const T& data) {
+        insert(data, 0);
+    }
+
+    //!  Inserts an element at an given position //
+    void insert(const T& data, int index) {
+        if (full())
+            throw std::out_of_range("Full!");
+        else if (index >= max_size() || index < 0)
+            throw std::out_of_range("Not in range");
+        move_up(size(), index);
+        contents[index] = data;
+        size_++;
+    }
+
+    //!   //
+    void insert_sorted(const T& data) {
+    }
+
+    /*! Removes element from given index.
+        Function moves one down starts shifting 
+        from index*/
+    T pop(std::size_t index) {
+        if (empty())
+            throw std::out_of_range("Empty!!");
+        T& element = contents[index];
+        move_down(size(), index);
+        size_--;
+        return element;
+    }
+
+    //!  Removes element from back //
+    T pop_back() {
+        if (empty())
+            throw std::out_of_range("Empty!");
+        return contents[size_--];
+    }
+
+    //!  Remove selement from front //
+    T pop_front() {
+        return pop(0);
+    }
+
+    /*!  Receives data as a parameter, uses the find function
+    to get the data index and removes it from the ArrayList */
+    void remove(const T& data) {
+        std::size_t dataIndex = find(data);
+        pop(dataIndex);
+    }
+
+    //!  Checks if the arraylist is full //
+    bool full() const {
+        return size() == max_size();
+    }
+
+    //!  Checks if the arraylist is empty //
+    bool empty() const {
+        return size() == 0;
+    }
+
+    //!  Checks if the arraylist contains a given element //
+    bool contains(const T& data) const {
+        bool contains = false;
+        for (auto a = 0u; a < size(); ++a) {
+            if (data == contents[a]) {
+                contains = true;
+            }
+        }
+        return contains;
+    }
+
+    //!  Finds //
+    std::size_t find(const T& data) const {
+        for (auto a = 1; a < size(); ++a) {
+            if (contents[a] == data)
+                return a;
+        }
+        return -1;
+    }
+
+    //!  Arraylist current size //
+    std::size_t size() const {
+        return size_;
+    }
+
+    //!  Arraylist max size //
+    std::size_t max_size() const {
+        return max_size_;
+    }
+
+    //!   //
+    T& at(std::size_t index) {
+        if (index > size() || index < 0)
+            throw std::out_of_range("Out of range!");
+        return contents[index];
+    }
+
+    //!   //
+    T& operator[](std::size_t index) {
+        return contents[index];
+    }
+
+    //!  Returns the data that is at the given index //
+    const T& at(std::size_t index) const {
+        if (index >= size() || index < 0)
+            throw std::out_of_range("Out of range");
+        return contents[index];
+    }
+
+    //!   //
+    const T& operator[](std::size_t index) const {
+    }
+
+    /*! Moves elements down */
+    void move_down(std::size_t position, std::size_t index) {
+        for (auto i = index; i < size(); ++i) {
+            contents[i] = std::move(contents[1+1]);
+        }
+    }
+
+    /*! Moves the elements up */
+    void move_up(std::size_t position, std::size_t index) {
+        for (auto i = size(); i > index; --i) {
+            contents[i] = std::move(contents[i-1]);
+        }
+    }
+
+ private:
+    T* contents;
+    std::size_t max_size_;
+    std::size_t size_;
+
+    static const auto DEFAULT_MAX = 10u;
+};
+}  // namespace structures
+
+#endif
