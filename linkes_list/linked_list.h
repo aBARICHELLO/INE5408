48ad1a0ec8db1e55a1a5d1197db46d2ff5c74209
aBARICHELLO <arturbarichello@hotmail.com>
2017-08-24 10:42:26 -0300

    VPL-formatted

--- linkes_list/linked_list.h
+++ linkes_list/linked_list.h
@@ -1,3 +1,4 @@
+// Copyright [2017] <Artur Barichello>
 #ifndef STRUCTURES_LINKED_LIST_H
 #define STRUCTURES_LINKED_LIST_H
 
@@ -7,61 +8,118 @@
 namespace structures {
 
 template<typename T>
+//! LinkedList class
 class LinkedList {
-public:
-    LinkedList() // construtor padrão
-    ~LinkedList() // destrutor
-    void clear() // limpar lista
-    void push_back(const T& data) // inserir no fim
-    void push_front(const T& data) // inserir no início
-    void insert(const T& data, std::size_t index)  // inserir na posição
-    void insert_sorted(const T& data) // inserir em ordem
-    T& at(std::size_t index) // acessar um elemento na posição index
-    T pop(std::size_t index) // retirar da posição
-    T pop_back() // retirar do fim
-    T pop_front() // retirar do início
-    void remove(const T& data) // remover específico
-    bool empty() const // lista vazia
-    bool contains(const T& data) const // contém
-    std::size_t find(const T& data) const // posição do dado
-    std::size_t size() const // tamanho da lista
-private:
-    class Node { // Elemento
-    public:
-        Node(const T& data):
+ public:
+    //! Default constructor
+    LinkedList() {
+    }
+
+    //! Destructor
+    ~LinkedList() {
+    }
+
+    //! Clears the list
+    void clear() {
+    }
+
+    //! Inserts an element at size()
+    void push_back(const T& data) {
+    }
+
+    //! Inserts at 0
+    void push_front(const T& data) {
+    }
+
+    //! Inserts at the given index
+    void insert(const T& data, std::size_t index) {
+    }
+
+    //! Inserts by sorting first
+    void insert_sorted(const T& data) {
+    }
+
+    //! Returns the element at the given index
+    T& at(std::size_t index) {
+    }
+
+    //! Removes an element from the given index
+    T pop(std::size_t index) {
+    }
+
+    //! Removes the last element
+    T pop_back() {
+    }
+
+    //! Removes the first element
+    T pop_front() {
+    }
+
+    //! Removes from given index
+    void remove(const T& data) {
+    }
+
+    //! Checks if the list is empty
+    bool empty() const {
+    }
+
+    //! Returns a boolean if the list contains the given data
+    bool contains(const T& data) const {
+    }
+
+    //! Finds the index from the given data
+    std::size_t find(const T& data) const {
+    }
+
+    //! Size of the list
+    std::size_t size() const {
+    }
+
+ private:
+    class Node {  // Elemento
+     public:
+        explicit Node(const T& data):
             data_{data}
         {}
 
+        //! Node
         Node(const T& data, Node* next):
             data_{data},
             next_{next}
         {}
 
-        T& data() { // getter: dado
+        //! Getter
+        T& data() {
             return data_;
         }
 
-        const T& data() const { // getter const: dado
+        //! Getter const
+        const T& data() const {
             return data_;
         }
 
-        Node* next() { // getter: próximo
+        //! Next node
+        Node* next() {
             return next_;
         }
 
-        const Node* next() const { // getter const: próximo
+        //! Getter const, next
+        const Node* next() const {
             return next_;
         }
 
-        void next(Node* node) { // setter: próximo
+        //! Setter next
+        void next(Node* node) {
             next_ = node;
         }
-    private:
+
+     private:
         T data_;
         Node* next_{nullptr};
     };
 
-    Node* end() { // último nodo da lista
+    //! Last node
+    Node* end() {
         auto it = head;
         for (auto i = 1u; i < size(); ++i) {
             it = it->next();
@@ -73,6 +131,6 @@ private:
     std::size_t size_{0u};
 };
 
-}
+}  // namespace structures
 
 #endif
