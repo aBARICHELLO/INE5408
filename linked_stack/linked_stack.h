18caec075d246a4ec4d3171821aa67b0a13c5f0b
aBARICHELLO <arturbarichello@hotmail.com>
2017-08-31 16:38:47 -0300

    Finished linked_stack

--- linked_stack/linked_stack.h
+++ linked_stack/linked_stack.h
@@ -1,71 +1,123 @@
 // Copyright [2017] <Artur Barichello>
+#ifndef STRUCTURES_LINKED_STACK_H
+#define STRUCTURES_LINKED_STACK_H
+
+#include <cstdint>
+#include <stdexcept>
+
 namespace structures {
-    
-    template<typename T>
-    //! LinkedStack class
-    class LinkedStack {
+
+template<typename T>
+//! LinkedStack class
+class LinkedStack {
+ public:
+    LinkedStack() {}
+
+    ~LinkedStack() {
+        clear();
+    }
+
+    //! Clears the list
+    void clear() {
+        auto current_node = top_;
+
+        for (auto i = 0u; i < size(); ++i) {
+            auto aux = current_node;
+            current_node = current_node->next();
+            delete aux;
+        }
+        size_ = 0;
+    }
+
+    //! Push
+    void push(const T& data) {
+        auto next_node = top_;
+        auto new_node = new Node{data, next_node};
+        top_ = new_node;
+        size_++;
+    }
+
+    //! Pop
+    T pop() {
+        if (empty()) {
+            throw std::out_of_range("Empty!");
+        }
+
+        auto temp_node = top_;
+        auto temp_data = temp_node->data();
+        top_ = top_->next();
+
+        --size_;
+        delete temp_node;
+        return temp_data;
+    }
+
+    //! Returns the data at the top of the Stack
+    T& top() const {
+        if (empty()) {
+            throw std::out_of_range("Empty!");
+        }
+        return top_->data();
+    }
+
+    //! Empty stack
+    bool empty() const {
+        return size() == 0;
+    }
+
+    //! Number of elements in the stack
+    std::size_t size() const {
+        return size_;
+    }
+
+ private:
+    //! Node class
+    class Node {
      public:
-        LinkedStack();
-    
-        ~LinkedStack();
-    
-        //! Clears the list
-        void clear() {
+        explicit Node(const T& data):
+            data_{data}
+        {}
+
+        //! Node
+        Node(const T& data, Node* next):
+            data_{data},
+            next_{next}
+        {}
+
+        //! Getter
+        T& data() {
+            return data_;
         }
-    
-        //! Push
-        void push(const T& data) {
+
+        //! Getter const
+        const T& data() const {
+            return data_;
         }
-    
-        //! Pop
-        T pop() {
+
+        //! Next node
+        Node* next() {
+            return next_;
         }
-    
-        //! Returns the data at the top of the Stack
-        T& top() const {
+
+        //! Getter const, next
+        const Node* next() const {
+            return next_;
         }
-    
-        //! Empty stack
-        bool empty() const {
+
+        //! Setter next
+        void next(Node* node) {
+            next_ = node;
         }
-    
-        //! Number of elements in the stack
-        std::size_t size() const {
-        }
-    
+
      private:
-        //! Node class
-        class Node {
-         public:
-            explicit Node(const T& data) {
-            }
-    
-            Node(const T& data, Node* next) {
-            }
-    
-            T& data() {
-            }
-    
-            const T& data() const {
-            }
-    
-            Node* next() {
-            }
-    
-            const Node* next() const {
-            }
-    
-            void next(Node* next) {
-            }
-    
-         private:
-            T data_;
-            Node* next_;
-        };
-    
-        Node* top_;
-        std::size_t size_;
+        T data_;
+        Node* next_;
     };
-    
-    }  // namespace structures
-    
+
+    Node* top_{nullptr};
+    std::size_t size_{0u};
+};
+
+}  // namespace structures
+
+#endif
