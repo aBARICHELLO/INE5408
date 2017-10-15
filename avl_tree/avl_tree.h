f1dc37f49e1b7de8a08ebdfebb35630c3d050fab
aBARICHELLO <arturbarichello@hotmail.com>
2017-10-15 13:20:12 -0200

    Unfinished methods

--- avl_tree/avl_tree.h
+++ avl_tree/avl_tree.h
@@ -3,6 +3,7 @@
 #ifndef STRUCTURES_AVL_TREE
 #define STRUCTURES_AVL_TREE
 
+#include <vector>
 #include <stdexcept>
 #include "array_list.h"
 
@@ -11,17 +12,17 @@ template<typename T>
 //! avl class
 class AVLTree {
 public:
-    //! Constructor
+    //! Destructor
     ~AVLTree();
 
     //! Insert
-    void insert(const T& data);
+    void insert(const T& node_data);
 
     //! Remov
-    void remove(const T& data);
+    void remove(const T& node_data);
 
     //! Contains
-    bool contains(const T& data) const;
+    bool contains(const T& node_data) const;
 
     //! Empty
     bool empty() const;
@@ -30,28 +31,90 @@ public:
     std::size_t size() const;
 
     //! Pre order
-    ArrayList<T> pre_order() const;
+    std::vector<T> pre_order() const {
+        auto vector = std::vector<T>{};
+        if (!empty()) {
+            root->pre_order(vector);
+        }
+        return vector;
+    }
 
     //! In order
-    ArrayList<T> in_order() const;
+    std::vector<T> in_order() const {
+        auto vector = std::vector<T>{};
+        if (!empty()) {
+            root->in_order(vector);
+        }
+        return vector;
+    }
 
     //! Post order
-    ArrayList<T> post_order() const;
+    std::vector<T> post_order() const {
+        auto vector = std::vector<T>{};
+        if (!empty()) {
+            root->post_order(vector);
+        }
+        return vector;
+    }
 
 private:
     //! Node struct
     struct Node {
         //! Node constructor
-        Node(const T& data);
+        explicit Node(const T& node_data):
+            node_data(node_data)
+        {}
+
+        //! Node destructor
+        ~Node() {
+            delete node_data;
+            if (left != nullptr) {
+                delete left;
+            }
+            if (right != nullptr) {
+                delete right;
+            }
+        }
 
         //! Node insert
-        void insert(const T& data_);
+        void insert(const T& data_) {
+            if (data_ <= node_data) {
+                if (left == nullptr) {
+                    left = new Node(data_);
+                } else {
+                    left->insert(data_);
+                }
+            } else {
+                if (right == nullptr) {
+                    right = new Node(data_);
+                } else {
+                    right->insert(data_);
+                }
+            }
+        }
 
         //! Node remove
-        bool remove(const T& data_);
+        bool remove(const T& data_) {
+            if (data_ == node_data) {
+                node_data = T{};
+                return true;
+            } else if (data_ < node_data) {
+                return (left) and left->remove(data_);
+            } else {
+                return (right) and right->remove(data_);
+            }
+        }
 
         //! Contains
-        bool contains(const T& data_) const;
+        bool contains(const T& data_) const {
+            if (data_ < node_data) {
+                return left == nullptr ? false : left->contains(data_);
+            } else if (data_ > node_data) {
+                return right == nullptr ? false : left->contains(data_);
+            } else {
+                return true;
+            }
+        }
 
         //! Height
         void updateHeight() ;
@@ -69,18 +132,42 @@ private:
         Node* doubleRight() ;
 
         //! Pre order
-        void pre_order(ArrayList<T>& v) const;
+        void pre_order(std::vector<T>& v) const {
+            v.push_back(node_data);
+            if (left) {
+                left->pre_order(v);
+            }
+            if (right) {
+                right->pre_order(v);
+            }
+        }
 
         //! In order
-        void in_order(ArrayList<T>& v) const;
+        void in_order(ArrayList<T>& v) const {
+            if (left) {
+                left->pre_order(v);
+            }
+            v.push_back(node_data);
+            if (right) {
+                right->pre_order(v);
+            }
+        }
 
         //! Post order
-        void post_order(ArrayList<T>& v) const;
+        void post_order(ArrayList<T>& v) const {
+            if (left) {
+                left->pre_order(v);
+            }
+            if (right) {
+                right->pre_order(v);
+            }
+            v.push_back(node_data);
+        }
 
-        T data;
-        std::size_t height;
-        Node* left;
-        Node* right;
+        T node_data;
+        std::size_t height{0};
+        Node* left{nullptr};
+        Node* right{nullptr};
     };
 
     Node* root;
